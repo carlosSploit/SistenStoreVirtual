d7682d3512f7c86b9c3c84279e681aafb346195c
carlosSploit <arturo14212000@gmail.com>
2021-07-08 20:58:17 -0500

    sdsfdssf

--- application/views/pie.php
+++ application/views/pie.php
@@ -32,7 +32,7 @@
 </a>
 
 <!-- Bootstrap core JavaScript-->
-<script src="<?php echo base_url() ?>vendor/twbs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url() ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
 <script src="<?php echo base_url() ?>vendor/jquery-easing/jquery.easing.min.js"></script>
